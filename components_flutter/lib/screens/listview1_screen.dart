a75c8f49076f8d2317b2bb9749834a24ed211f42
Gomez Enzo <gomezenzo1128@gmail.com>
2022-07-06 16:32:52 -0300

    Proyect Start

---
+++ components_flutter/lib/screens/listview1_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class Listview1Screen extends StatelessWidget {
+  final options = const ['megaman', 'metal Gear'];
+
+  const Listview1Screen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Center(child: Text("Listview Tipo 1")),
+          elevation: 0,
+          backgroundColor: Colors.indigo,
+        ),
+        body: ListView.separated(
+          itemCount: options.length,
+          itemBuilder: (context, index) => ListTile(
+            title: Text(options[index]),
+            trailing: const Icon(
+              Icons.arrow_forward_ios_outlined,
+              color: Colors.indigo,
+            ),
+            onTap: () {
+              final game = options[index];
+              print(game);
+            },
+          ),
+          separatorBuilder: (_, __) => const Divider(),
+        ));
+  }
+}
---
+++ components_flutter/lib/screens/screens.dart
@@ -0,0 +1,4 @@
+export 'package:components_flutter/screens/alert_screen.dart';
+export 'package:components_flutter/screens/cards_screen.dart';
+export 'package:components_flutter/screens/listview1_screen.dart';
+export 'package:components_flutter/screens/home_screen.dart';
